5232f82c3157f9c018c9af8dae2a6aef7fec1501
Ghost1395 <70482034+Ghost1395@users.noreply.github.com>
2020-09-17 01:10:55 +0600

    finel product page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function myFunction2() {
+  document.getElementById("myDropdown-2").classList.toggle("show");
+}
+
+function myFunction3() {
+  document.getElementById("myDropdown-3").classList.toggle("show");
+}
+
